1fdee8ab2c328117eb882eb007508ba1caa3abb6
Tanisha-221 <tanishajindal223@gmail.com>
2026-01-21 17:48:15 +0530

    changed the name of acr registry

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "random_integer" "ri" {
 # Azure Container Registry
 # ---------------------------
 resource "azurerm_container_registry" "example" {
-  name                = "sreacregistry13579063"
+  name                = "sreacregistry135790631"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   sku                 = "Standard"
